e3ff3cf37479783ae52f419f46d391bc3cde007d
riodelphino <ryo.heavenlysky@gmail.com>
2026-01-21 21:35:15 +0900

    perf: Include type definitions into `init.lua` for better performance

--- lua/macime/init.lua
+++ lua/macime/init.lua
@@ -1,4 +1,23 @@
-require('macime.types')
+---@class macime.Config.Save
+---@field global? boolean
+
+---@class macime.Config.Service
+---@field enabled? boolean
+---@field sock_path? string
+
+---@class macime.Config.Ime
+---@field default? string
+
+---@class macime.Config.Exclude
+---@field filetype? string[]
+
+---@class macime.Config
+---@field ttimeoutlen? number
+---@field ime? macime.Config.Ime
+---@field save? macime.Config.Save
+---@field service? macime.Config.Service
+---@field pattern? string|string[]
+---@field exclude? macime.Config.Exclude
 
 local M = {}
 
